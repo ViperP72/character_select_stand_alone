cbd95aabbb6b96ca870cd87d5e8be9ae49865f29
ViperP72 <patrykvi@yahoo.com>
2025-02-28 11:00:44 +0100

    Use venv + add missing OpenCV requirements

--- #run_EN.bat
+++ #run_EN.bat
@@ -1,4 +1,13 @@
 @echo off
 @set GRADIO_SERVER_PORT=47861
+set "VENV_DIR=%~dp0%env"
+if EXIST "%VENV_DIR%" goto :start
+cd "%~dp0%"
+python -m venv env
+call "%VENV_DIR%\Scripts\activate.bat"
+python -m pip install -r requirements.txt --upgrade pip
+:start
+python -m venv env
+call "%VENV_DIR%\Scripts\activate.bat"
 py -m app --english True
 pause
